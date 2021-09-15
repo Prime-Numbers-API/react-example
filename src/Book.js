20d53de4e7d500da5ffd4060e4e1560504437e17
Evan Poe <evanpoe91@yahoo.com>
2021-09-15 11:39:13 -0500

    initial react application

--- src/Book.js
+++ src/Book.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
-class Number extends React.Component {
+class Book extends React.Component {
     render() {
         //get the arrays of authors
         const authors = this.props.author
         //get the saleability status
         const saleability = this.props.saleability
 
-        //if the number is free ...
+        //if the book is free ...
         if (saleability === 'FREE') {
             //... there is no price to show
             return (
                 <div className="search-results-item">
                     <h2>{this.props.title}</h2>
                     <a href={this.props.previewLink}>
-                        <img className='numberImage'
+                        <img className='bookImage'
                             src={this.props.thumbnail_URL}
-                            alt='numberimage' />
+                            alt='bookimage' />
                     </a>
 
                     <div>
@@ -28,7 +28,7 @@ class Number extends React.Component {
                 </div>
             )
         }
-        //if the number is NOT free ...
+        //if the book is NOT free ...
         else {
             //... get the price details
             const price = this.props.price
@@ -40,7 +40,7 @@ class Number extends React.Component {
                     <h2>{this.props.title}</h2>
                     <h2>{this.props.publisher}</h2>
                     <a href={this.props.previewLink} target='_blank' rel='noopener noreferrer'>
-                        <img className='numberImage' src={this.props.thumbnail_URL} alt='numberimage' />
+                        <img className='bookImage' src={this.props.thumbnail_URL} alt='bookimage' />
                     </a>
                     <div>
                         <h3>Authors: {authors}</h3>
@@ -55,4 +55,4 @@ class Number extends React.Component {
 
 }
 
-export default Number
+export default Book
