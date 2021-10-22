c75f96052d340eff014480ed9f5d4a343749afc9
Evan Poe <evanpoe91@yahoo.com>
2021-10-22 16:52:15 -0500

    red error flag rendering correctly. 'X' button not looking correct, but closes error correctly.  However, error does not re render if incorrect information passed after closing error (unless DOM is refreshed)

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -4,13 +4,19 @@ export default class ErrorHandler extends Component {
     render() {
         const content = this.props.content
         console.log(content)
-
+        
         let errorMessage = "";
 
         for (let key in content) {
             console.log(content[key]);
             if (typeof content[key] == "object") {
-                errorMessage += `<div className="alert alert-danger alert-dismissible show-error error-is-this-number-prime" role="alert"> <button type="button" className="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button> <strong>${content[key]}</strong> ` 
+                errorMessage += ` <strong>${content[key]}:  Something went wrong...</strong> ` 
+
+                let errorComponent = ` <button type="button" className="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button> ${errorMessage}`;
+
+                return (
+                    <div className="alert alert-danger alert-dismissible show-error error-is-this-number-prime" role="alert" dangerouslySetInnerHTML={{__html: errorComponent }} />
+                )
                 
             }  if (typeof content[key] == false) {
                 errorMessage += "";
@@ -18,7 +24,7 @@ export default class ErrorHandler extends Component {
         }
         return (
             <>
-                <div dangerouslySetInnerHTML={{__html: errorMessage }} />
+                <div  dangerouslySetInnerHTML={{__html: errorMessage }} />
             </>
         )
     }
